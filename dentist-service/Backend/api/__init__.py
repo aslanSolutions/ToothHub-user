9936e389210830c33d5b538273d9577a540d5073
M.Ali Elhasan <guselhmu@student.gu.se>
2023-11-20 11:45:12 +0100

    Restrucure repo and implement the endpoint for dentist service #8 #3 #11

---
+++ dentist-service/Backend/api/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from apifairy import APIFairy
+from flask_marshmallow import Marshmallow
+from .routes import bp
+
+apifairy = APIFairy()
+ma = Marshmallow()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['APIFAIRY_TITLE'] = 'Dentist API'
+    app.config['APIFAIRY_VERSION'] = '1.0'
+
+    # Initialize APIFairy, Marshmallow
+    apifairy.init_app(app)
+    ma.init_app(app)
+
+    # Register the dentsit blueprint
+    app.register_blueprint(bp)
+
+    return app
