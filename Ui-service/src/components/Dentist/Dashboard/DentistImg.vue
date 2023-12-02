25afe2019f31aa81b234a4718df343a004264560
yousefab <yousefab@net.chalmers.se>
2023-12-02 18:38:14 +0100

    Responsiveness Issue Addressed #16

--- Ui-service/src/components/Dentist/Dashboard/DentistImg.vue
+++ Ui-service/src/components/Dentist/Dashboard/DentistImg.vue
@@ -1,14 +1,24 @@
 <template>
-
     <div class="dentist-img">
+      <div class="overlay-container">
         <img src="../../../assets/doctor.png" alt="doctor">
+        <div class="text-container">
+          <h1 class="text-4xl md:text-4xl leading-24 md:leading-35 font-inter font-semibold tracking-tighter text-stone-50	">
+            Welcome, Dr. Sarah
+          </h1>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <style lang="scss" scoped>
 
-<script>
-
-</script>
-
-<style lang="scss">
-
-</style>
+  
+    .text-container {
+      position: relative;
+      transform: translate(5%, -350%);
+      text-align: center; /* Center text within the container */
+      width: 100%;
+    }
+  </style>
+  
