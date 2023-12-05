ef9b97f8f73b40085e40b536c03584c9b060b93b
Aslan <muhmmad@student.chalmers.se>
2023-12-05 21:47:42 +0100

    #22 added update route

--- Ui-service/src/api/serviceApi.js
+++ Ui-service/src/api/serviceApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api/v1/services'
+const BASE_URL = 'http://localhost:5000/services'
 
 export const services = {
   getAllServices() {
--- dentist-service/api/service_routes.py
+++ dentist-service/api/service_routes.py
@@ -85,3 +85,42 @@ def delete_service(service_id):
         return {"message":"Service deleted successfully"}
     else:
         return jsonify({'message': 'Service not found'}), 404
+
+
+@service_bp.route('/<service_id>', methods=['PUT'])
+@response(service_schema, 200)
+@body(service_schema)
+@other_responses({400: 'Invalid request.', 404: 'Service not found.'})
+def update_service(data, service_id):
+    """Update a service by ID"""
+    try:
+        # Convert service_id to ObjectId
+        service_object_id = ObjectId(service_id)
+
+        # Check if the service exists
+        existing_service = services.find_one({'_id': service_object_id})
+        if existing_service is None:
+            return {'message': 'Service not found.'}, 404
+
+        # Access the request payload directly
+        if 'image' in request.files:
+            file = request.files['image']
+            # Read the file data as binary
+            image_data = file.read()
+            image_data_base64 = base64.b64encode(image_data).decode('utf-8')
+            # Set the 'image' field in the data to the binary image data
+            data['image'] = image_data_base64
+
+        # Update the service by its ID with the updated data
+        services.update_one({'_id': service_object_id}, {'$set': data})
+
+        # Retrieve the updated service
+        updated_service = services.find_one({'_id': service_object_id})
+
+        # Convert binary image data to base64 before returning the response
+        if 'image' in updated_service and isinstance(updated_service['image'], bytes):
+            updated_service['image'] = base64.b64encode(updated_service['image']).decode('utf-8')
+
+        return updated_service
+    except Exception as e:
+        return {'error': str(e)}, 404
