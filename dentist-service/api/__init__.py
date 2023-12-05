1d1cd3984dacf1016666da4eef4794a12a0fa5d4
Aslan <muhmmad@student.chalmers.se>
2023-12-05 22:55:19 +0100

    #22 used cores in the server

--- dentist-service/api/__init__.py
+++ dentist-service/api/__init__.py
@@ -4,6 +4,7 @@ from flask_marshmallow import Marshmallow
 from .routes import bp
 from .service_routes import service_bp
 from .marsh_schema import ma
+from flask_cors import CORS
 
 apifairy = APIFairy()
 ma = Marshmallow()
@@ -13,9 +14,10 @@ def create_app():
     app.config['APIFAIRY_TITLE'] = 'Dentist API'
     app.config['APIFAIRY_VERSION'] = '1.0'
 
-    # Initialize APIFairy, Marshmallow
+    # Initialize APIFairy, Marshmallow, and CORS
     apifairy.init_app(app)
     ma.init_app(app)
+    CORS(app, origins="http://localhost:8080")
 
     # Register the dentsit blueprint
     app.register_blueprint(bp)
