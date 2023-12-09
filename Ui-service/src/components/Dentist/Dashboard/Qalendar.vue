fdc2c3504123689d4265559b4f8f51300aa2c753
elhasan <muhammadaliallhassan37@gmail.com>
2023-12-09 20:15:24 +0100

    Dentist Calander

--- Ui-service/src/components/Dentist/Dashboard/Qalendar.vue
+++ Ui-service/src/components/Dentist/Dashboard/Qalendar.vue
@@ -1,9 +1,8 @@
 <template>
-  <vue-cal id="cal" hide-view-selector hide-title-bar hide-weekends :time-from="8 * 60" :time-to="18 * 60" :time-step="30"
-    :disable-views="['years', 'year']"
+  <vue-cal ref="calendar" id="cal" hide-view-selector hide-title-bar hide-weekends :time-from="8 * 60" :time-to="18 * 60"
+    :time-step="30" :disable-views="['years', 'year']"
     :editable-events="{ title: false, drag: true, resize: true, delete: true, create: true }"
-    :drag-to-create-threshold="0" @event-create="handleEventCreate" @event-drop="handleEventDrop"
-    @event-resize="handleEventResize">
+    :drag-to-create-threshold="0" @event-drop="handleEventDrop" @event-resize="handleEventResize" @event-drag="handleCellClick">
     ></vue-cal>
 </template>
 
@@ -18,14 +17,6 @@ export default {
     VueCal
   },
   props: {
-    timeFrom: {
-      type: Number,
-      default: 480 // Default start time (e.g., 8 AM if using minutes from midnight)
-    },
-    timeTo: {
-      type: Number,
-      default: 1020 // Default end time (e.g., 5 PM if using minutes from midnight)
-    }
   },
   data() {
     return {
@@ -37,77 +28,6 @@ export default {
     showErrorToUser(message) {
       // Display the error message to the user using an alert or another UI element
       alert(message);
-    },
-    handleEventCreate(event) {
-      // Initial start and end times
-      let startTime = event.start;
-      let endTime = event.end;
-
-      // Variables to store initial drag positions
-      let startX;
-      let startY;
-
-
-      // Listen for drag events on the calendar
-      const calendar = document.querySelector('#cal'); // Ensure correct selector
-      this.dragging = false;
-      calendar.addEventListener('mousedown', (e) => {
-        startX = e.offsetX;
-        startY = e.offsetY;
-        this.dragging = true;
-      });
-      // Listen for mouse move events during dragging
-      document.addEventListener('mousemove', (e) => {
-        if (!this.dragging) return;
-
-        const x = e.offsetX;
-        const y = e.offsetY;
-
-        // Update the event's start and end times based on drag positioning
-        const currentTime = new Date();
-        let newStartTime = new Date(startTime);
-        let newEndTime = new Date(endTime);
-
-        if (x < startX) {
-          newStartTime.setHours(currentTime.getHours() - 1);
-        } else if (x > startX + event.width) {
-          newStartTime.setHours(currentTime.getHours() + 1);
-        }
-
-        if (y < startY) {
-          newStartTime.setMinutes(currentTime.getMinutes() - 15);
-          newStartTime.setSeconds(0);
-        } else if (y > startY + event.height) {
-          newStartTime.setMinutes(currentTime.getMinutes() + 15);
-          newStartTime.setSeconds(0);
-        }
-
-        newEndTime.setMinutes(newStartTime.getMinutes() + event.duration);
-
-        // Update the event object with the new times
-        event.start = newStartTime.toISOString();
-        event.end = newEndTime.toISOString();
-      });
-
-      // Listen for mouse up events to stop dragging
-      document.addEventListener('mouseup', () => {
-        this.dragging = false;
-
-        // Convert the start and end times to ISO string
-        const startDate = event.start.toISOString().split('T')[0];
-        const startTime = event.start.toISOString();
-        const endTime = event.end.toISOString();
-
-        // Prepare the new time slot object
-        const newTimeSlot = {
-          time_slot_id: `slot_${Date.now()}`,
-          start_time: startTime,
-          end_time: endTime,
-        };
-
-        // Once dragging ends, send the updated event to backend
-        this.setAvailability("dentistEmail",startDate, [newTimeSlot]);
-      });
     },
     // Called when an existing event is dropped after dragging
     handleEventDrop({ event, newStart, newEnd }) {
@@ -194,22 +114,20 @@ export default {
         // Handle the invalid data appropriately, e.g., show an error message
         return;
       }
-
+      console.log("Test")
       // Convert the start and end to ISO strings
       const startTime = eventDetails.start.toISOString();
       const endTime = eventDetails.end.toISOString();
 
       // Create the time slot data
-      const timeSlots = [
-        {
-          time_slot_id: '1', // You can generate a unique ID here
-          start_time: startTime,
-          end_time: endTime,
-        },
-      ];
+      const timeSlot = {
+        time_slot_id: `slot_${Date.now()}`, // You can generate a unique ID here
+        start_time: startTime,
+        end_time: endTime,
+      };
 
       // Now, call setAvailability with the necessary data
-      this.setAvailability('john@example.com', startTime.split('T')[0], timeSlots);
+      this.setAvailability('dentistEmail', startTime.split('T')[0], [timeSlot]);
     },
     fetchEvents() {
       // Fetching logic
