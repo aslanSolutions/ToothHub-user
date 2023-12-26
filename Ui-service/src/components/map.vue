6a980f4392e57f7ea627978f7e58865dff19a8df
yosuefab <yousefab@net.chalmers.se>
2023-12-26 17:19:28 +0100

    Implementation of mapbox #24

--- Ui-service/src/components/map.vue
+++ Ui-service/src/components/map.vue
@@ -1,20 +1,70 @@
 <template>
-    <GMap
-      :center="{lat: 37.7749, lng: -122.4194}"
-      :zoom="12"
-      style="width: 100%; height: 400px"
-    >
-    </GMap>
+    <div>
+      <MglMap
+        :accessToken="mapboxAccessToken"
+        :mapStyle="mapStyle"
+        :center="center"
+        :zoom="zoom"
+        style="height: 400px; width: 100%;"
+      >
+        <MglMarker
+          v-for="clinic in clinics"
+          :key="clinic.id"
+          :lngLat="[clinic.longitude, clinic.latitude]"
+        >
+          <div>{{ clinic.name }}</div>
+        </MglMarker>
+      </MglMap>
+    </div>
   </template>
   
   <script>
-  import { GMap } from 'vue3-google-maps'
-  import axios from axios
-
-
-
-
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  import { MglMap, MglMarker } from 'vue-mapbox';
   
-
+  const BASE_URL = 'http://localhost:5000/clinics/';
+  
+  export default {
+    components: {
+      MglMap,
+      MglMarker,
+    },
+    setup() {
+      const clinics = ref([]);
+  
+      onMounted(async () => {
+        try {
+          clinics.value = await getAllClinics();
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      });
+  
+      return { clinics };
+    },
+    methods: {
+      getAllClinics: async () => {
+        try {
+          const response = await axios.get(BASE_URL);
+          return response.data;
+        } catch (error) {
+          console.error('Error fetching clinics:', error);
+          throw error;
+        }
+      },
+    },
+    data() {
+      return {
+        mapboxAccessToken: 'pk.eyJ1IjoiZ3VjY2licmFnYSIsImEiOiJjbHFtamY1M28yajI3Mml0a3BpMjF2a3BhIn0.0WPtfrTGre_3Y-NYQQfw3A', 
+        mapStyle: 'mapbox://styles/mapbox/streets-v11',
+        center: [57.7, 11.9],
+        zoom: 9,
+      };
+    },
+  };
   </script>
+  
+  <style>
+  </style>
   
--- Ui-service/src/main.js
+++ Ui-service/src/main.js
@@ -1,20 +1,19 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css'
-
-
-
 import router from './router'
 import store from './store';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { MglMap, MglMarker, MglNavigationControl, MglGeolocateControl } from 'vue-mapbox';
+
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
-app.use(VueGoogleMaps, {
-    load: {
-      key: 'AIzaSyA70fTDcp3dluL9ScZ0nOi7xc25tuwUIDc',
-    },
-  })
+app.component('MglMap', MglMap);
+app.component('MglMarker', MglMarker);
+app.component('MglNavigationControl', MglNavigationControl);
+app.component('MglGeolocateControl', MglGeolocateControl);
 
 
 
