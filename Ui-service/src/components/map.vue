5711ebd767c69cb28c6ff60e7ff0cab9f20e2c1b
yosuefab <yousefab@net.chalmers.se>
2023-12-26 16:19:40 +0100

    Map component #24

---
+++ Ui-service/src/components/map.vue
@@ -0,0 +1,20 @@
+<template>
+    <GMap
+      :center="{lat: 37.7749, lng: -122.4194}"
+      :zoom="12"
+      style="width: 100%; height: 400px"
+    >
+    </GMap>
+  </template>
+  
+  <script>
+  import { GMap } from 'vue3-google-maps'
+  import axios from axios
+
+
+
+
+  
+
+  </script>
+  
--- Ui-service/src/main.js
+++ Ui-service/src/main.js
@@ -3,11 +3,20 @@ import App from './App.vue';
 import './index.css'
 
 
+
 import router from './router'
 import store from './store';
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyA70fTDcp3dluL9ScZ0nOi7xc25tuwUIDc',
+    },
+  })
+
+
+
 
 app.mount('#app');
